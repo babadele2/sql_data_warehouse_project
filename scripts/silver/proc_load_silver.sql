90acc1491ecef502c030c4acb5aa6d07f79d0ad2
babadele2 <81587982+babadele2@users.noreply.github.com>
2025-06-20 21:13:29 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -56,6 +56,38 @@ FROM (
 
 
 
+-- Loading silver.crm_prd_info
+       
+TRUNCATE TABLE silver.crm_prd_info;
+    
+INSERT INTO silver.crm_prd_info (
+	prd_id, 
+	cat_id, 
+	prd_key, 
+	prd_nm, 
+	prd_cost,
+        prd_line, 
+	prd_start_dt, 
+	prd_end_dt
+)
+SELECT
+    prd_id,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_'),
+SUBSTRING(prd_key, 7),
+    prd_nm,
+COALESCE(prd_cost, 0),
+CASE 
+    WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+    WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+    WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+    WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+    ELSE 'n/a'
+    END,
+    prd_start_dt,
+    (LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day')::DATE
+    FROM bronze.crm_prd_info;
+
+
 
 
 
